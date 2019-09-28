5429701be13b395f20a0c11c567f9f136bb194b5
Nemanja Simeunovic <nem@nemsim.com>
2019-09-28 18:01:23 -0500

    updating to v5

--- keyboards/preonic/keymaps/nemsim/keymap.c
+++ keyboards/preonic/keymaps/nemsim/keymap.c
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * | Esc  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Del  |
+ * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  |  \   |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | Ctrl |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_QWERTY] = {
   {KC_ESC        , KC_1    , KC_2    , KC_3    , KC_4  , KC_5   , KC_6   , KC_7  , KC_8    , KC_9    , KC_0    , KC_BSPC} ,
-  {KC_TAB        , KC_Q    , KC_W    , KC_E    , KC_R  , KC_T   , KC_Y   , KC_U  , KC_I    , KC_O    , KC_P    , KC_DEL}  ,
+  {KC_TAB        , KC_Q    , KC_W    , KC_E    , KC_R  , KC_T   , KC_Y   , KC_U  , KC_I    , KC_O    , KC_P    , KC_BSLS}  ,
   {CTL_T(KC_ESC) , KC_A    , KC_S    , KC_D    , KC_F  , KC_G   , KC_H   , KC_J  , KC_K    , KC_L    , KC_SCLN , KC_QUOT} ,
   {KC_LSFT       , KC_Z    , KC_X    , KC_C    , KC_V  , KC_B   , KC_N   , KC_M  , KC_COMM , KC_DOT  , KC_SLSH , KC_ENT } ,
   {MO(_LAYER_FN) , KC_LCTL , KC_LGUI , KC_LALT , LOWER , KC_SPC , KC_SPC , RAISE , KC_LEFT , KC_DOWN , KC_UP   , KC_RGHT}
@@ -53,43 +53,43 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Lower
  * ,-----------------------------------------------------------------------------------.
- * |   ~  |   7  |   8  |   9  |   /  |      |      |      |      |      |      |      |
+ * |   ~  |      |      |      |      |      |      |   7  |   8  |   9  |   /  | Del  |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Enter|   4  |   5  |   6  |   *  |      |   _  |   *  |   /  |      |      |      |
+ * | Enter|      |      |      |      |   \  |      |   4  |   5  |   6  |   *  |      |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |   1  |   2  |   3  |   -  |      |   =  |   +  |   -  |      |      |      |
+ * |      |   _  |   -  |   +  |   =  |   |  |      |   1  |   2  |   3  |   -  |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |   0  |   .  |   =  |   +  |      |      |      |      |      |      |      |
+ * |      |      |      |      |      |      |      |   0  |   =  |   .  |   +  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |    Bkspc    |      | Next | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = {
-  {KC_TILD,   KC_P7,   KC_P8,   KC_P9, KC_PSLS, _______, _______, _______, _______, _______, _______, _______},
-  {KC_PENT,   KC_P4,   KC_P5,   KC_P6, KC_PAST, _______, KC_UNDS, KC_ASTR, KC_SLSH, _______, _______, _______},
-  {_______,   KC_P1,   KC_P2,   KC_P3, KC_PMNS, _______,  KC_EQL, KC_PLUS, KC_MINS, _______, _______, _______},
-  {_______,   KC_P0, KC_PDOT, KC_PEQL, KC_PPLS, _______, _______, _______, _______, _______, _______, _______},
+  {KC_TILD, _______, _______, _______, _______, _______, _______,   KC_P7,   KC_P8,   KC_P9, KC_PSLS,  KC_DEL},
+  {KC_PENT, _______, _______, _______, _______, KC_BSLS, _______,   KC_P4,   KC_P5,   KC_P6, KC_PAST, _______},
+  {_______, KC_UNDS, KC_MINS, KC_PLUS,  KC_EQL, KC_PIPE, _______,   KC_P1,   KC_P2,   KC_P3, KC_PMNS, _______},
+  {_______, _______, _______, _______, _______, _______, _______,   KC_P0, KC_PEQL, KC_PDOT, KC_PPLS, _______},
   {_______, _______, _______, _______, _______, KC_BSPC, KC_BSPC, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY}
 },
 
 /* Raise
  * ,-----------------------------------------------------------------------------------.
- * |   `  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  |      |
+ * |   `  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Del  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |   <  |   {  |   }  |   >  |      |      |      |      |
+ * |      |      |      |   {  |   }  |      |      |   (  |   )  |      |      |      |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |      |      |      |   (  |   )  |   |  |   \  |      |   :  |   "  |
+ * |      |      |      |   [  |   ]  |      | Left | Down |  Up  | Rght |   :  |   "  |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |      |      |      |      |   [  |   ]  |      |   <  |   >  |   ?  |      |
+ * |      |      |      |      |      |      |      |      |   <  |   >  |   ?  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |    Enter    |      | Next | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = {
-  { KC_GRV, KC_EXLM,   KC_AT, KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______},
-  {_______, _______, _______, _______,   KC_LT, KC_LCBR, KC_RCBR,   KC_GT, _______, _______, _______, _______},
-  {_______, _______, _______, _______, _______, KC_LPRN, KC_RPRN, KC_PIPE, KC_BSLS, _______, KC_COLN, KC_DQT},
-  {_______, _______, _______, _______, _______, KC_LBRC, KC_RBRC, _______,   KC_LT,   KC_GT, KC_QUES, _______},
+  { KC_GRV, KC_EXLM,   KC_AT, KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,  KC_DEL},
+  {_______, _______, _______, KC_LCBR, KC_RCBR, _______, _______, KC_LPRN, KC_RPRN, _______, _______, _______},
+  {_______, _______, _______, KC_LBRC, KC_RBRC, _______, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, KC_COLN,  KC_DQT},
+  {_______, _______, _______, _______, _______, _______, _______, _______,   KC_LT,   KC_GT, KC_QUES, _______},
   {_______, _______, _______, _______, _______,  KC_ENT,  KC_ENT, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY}
 },
 
@@ -107,7 +107,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] = {
-  {KC_F1,     KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12},
+  {  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12},
   {_______,   RESET, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
   {_______, _______, _______,   AU_ON,  AU_OFF, AG_NORM, AG_SWAP, _______, _______, _______, _______, _______},
   {_______,  MUV_DE,  MUV_IN,   MU_ON,  MU_OFF,   MI_ON,  MI_OFF, _______, _______, _______, _______, _______},
@@ -130,26 +130,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LAYER_FN] = {
   {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
   {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
-  {_______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______},
+  {_______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, _______, _______},
   {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
   {KC_TRNS, _______, _______, _______, _______, KC_ENT,  KC_ENT,  _______, _______, _______, _______, _______}
 }
 
 };
-
-#ifdef AUDIO_ENABLE
-// Beethoven's 5th-ish
-float tone_startup[][2] = {
-  {NOTE_E4, 20},
-  {NOTE_E4, 20},
-  {NOTE_E4, 20},
-  {NOTE_C4, 40}
-};
-
-float tone_goodbye[][2]    = SONG(GOODBYE_SOUND);
-
-float music_scale[][2]     = SONG(MUSIC_SCALE_SOUND);
-#endif
 
 void persistant_default_layer_set(uint16_t default_layer) {
   eeconfig_update_default_layer(default_layer);
@@ -192,37 +178,3 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
     return true;
 };
-
-void matrix_init_user(void) {
-    #ifdef AUDIO_ENABLE
-        startup_user();
-    #endif
-}
-
-#ifdef AUDIO_ENABLE
-
-void startup_user()
-{
-    _delay_ms(20); // gets rid of tick
-    // play startup tone
-    /* PLAY_NOTE_ARRAY(tone_startup, false, 0.25); */
-}
-
-void shutdown_user()
-{
-    PLAY_NOTE_ARRAY(tone_goodbye, false, 0.25);
-    _delay_ms(150);
-    stop_all_notes();
-}
-
-void music_on_user(void)
-{
-    music_scale_user();
-}
-
-void music_scale_user(void)
-{
-    PLAY_NOTE_ARRAY(music_scale, false, 0.25);
-}
-
-#endif
--- keyboards/preonic/keymaps/nemsim/readme.md
+++ keyboards/preonic/keymaps/nemsim/readme.md
@@ -1,3 +1,7 @@
 # Nem's WIP Preonic layout - largely based on the Default Planck's
 
-[keyboard layout editor](http://www.keyboard-layout-editor.com/#/gists/661280cd18f605646c25acc7768aa74f)
+[keyboard layout editor](http://www.keyboard-layout-editor.com/#/gists/9620001c3acdde84dc70322c688cd3df)
+
+# Build
+
+`make preonic/rev2:nemsim:dfu`
